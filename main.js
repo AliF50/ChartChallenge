0be592ae41386730bd19e8aeada5ec17ae1cb4fc
Ali Hassan <alihassan@post.com>
2017-06-08 19:17:47 -0400

    Finishing touches

--- main.js
+++ main.js
@@ -9,28 +9,29 @@ app.controller('MainController', function($scope) {
         },
         backgroundColor: '#fff',
         plotarea: {
-            "margin-top": '50px',
+            "margin-top": '50px', //positioning of graph itself
             "margin-left": "20%"
         },
         legend: {
-            "layout": "6x1",
-            "align": "left",
-            "vertical-align": "top",
-            "toggle-action": "hide",
-            "highlight-plot": true,
+            "layout": "6x1", //make it 6 rows by 1 column
+            "align": "left", //position of legend
+            "vertical-align": "top", //position of legend
+            "toggle-action": "hide", //hides series if clicked on
+            "highlight-plot": true, //highlights the series if mouse hovers over
         },
         "scale-x": {
             "min-value": new Date().getTime(),
-            step: "1second",
+            step: "1250", //every x value will be 1250s
             transform: {
-                type: "date",
+                type: "date", //type of date
                 all: "%h:%i:%s" //format to be present in the x-axis
             },
             item: {
                 "font-size": 9
-            }
+            },
+            "max-items": 4 //display maximum 4 values on the x-axis
         },
-        crosshairX: {
+        crosshairX: { //options for crosshair when user hovers over points in graph
             lineWidth: 4,
             lineStyle: 'dashed',
             lineColor: '#424242',
@@ -56,13 +57,13 @@ app.controller('MainController', function($scope) {
         },
         scaleY: {
             guide: {
-                "line-color": "black",
+                "line-color": "black", //main guides every 20 units
                 "line-width": 2,
                 "line-style": "solid" //"solid", "dotted", "dashed", "dashdot"
             },
-            values: '0:100:20',
-            "minor-ticks": 3,
-            "minor-guide": {
+            values: '0:100:20', //go from 0 to 100, put a guide every 20 units
+            "minor-ticks": 3, //3 minor ticks between each guide
+            "minor-guide": { //styling of minor guides
                 "line-color": "grey",
                 "line-width": 1,
                 "line-style": "solid",
@@ -73,25 +74,27 @@ app.controller('MainController', function($scope) {
             visible: false
         },
         //real-time feed
-        refresh: {
+        refresh: { //gets realtime data from window.feed function
             type: 'feed',
             transport: 'js',
             url: 'feed()',
-            interval: 1250
+            interval: 1250, //update every 1.25seconds
+            'adjust-scale': true, //make graph start from the left
+            resetTimeout: 1000 //used to create a flow of data and no refreshes, default is after 100 points, it refereshes
         },
-        plot: {
+        plot: { //properties of the plot function
             shadow: 1,
             shadowColor: '#eee',
             shadowDistance: '10px',
             lineWidth: 5,
             hoverState: { visible: false },
             marker: { visible: false },
-            aspect: 'spline',
+            aspect: 'spline', //spline to make smooth curves
             "highlight-state": {
                 "line-width": "10 px"
             },
         },
-        series: [{
+        series: [{ //all of the series data
             values: [],
             lineColor: 'teal',
             text: '211'
@@ -113,19 +116,19 @@ app.controller('MainController', function($scope) {
             text: '215'
         }, {
             values: [],
-            lineColor: 'Orange',
+            lineColor: 'orange',
             text: '216'
         }]
     };
 
     //real-time feed random math function
     window.feed = function(callback) {
-        var xValue = new Date().getTime() / 1000;
-        var sinValue = Math.sin(0.4 * Math.PI * xValue);
+        var xValue = new Date().getTime() / 1000; //make the sin value as a function of time in seconds
+        var sinValue = Math.sin(0.4 * Math.PI * xValue); //get the value of sin
         var tick = {}; //sin(0.4*PI*x)
-        tick.plot0 = parseInt((85 + 100) / 2 + 7.5 * Math.random() * sinValue, 10);
-        tick.plot1 = parseInt((75 + 90) / 2 + 7.5 * Math.random() * sinValue, 10);
-        tick.plot2 = parseInt((65 + 80) / 2 + 7.5 * Math.random() * sinValue, 10);
+        tick.plot0 = parseInt((85 + 100) / 2 + 7.5 * Math.random() * sinValue, 10); //some math to determine the 
+        tick.plot1 = parseInt((75 + 90) / 2 + 7.5 * Math.random() * sinValue, 10); //axis of symmetry and
+        tick.plot2 = parseInt((65 + 80) / 2 + 7.5 * Math.random() * sinValue, 10); //amplitude (amplitude is random between 0-7.5)
         tick.plot3 = parseInt((55 + 75) / 2 + 7.5 * Math.random() * sinValue, 10);
         tick.plot4 = parseInt((50 + 70) / 2 + 7.5 * Math.random() * sinValue, 10);
         tick.plot5 = parseInt((45 + 65) / 2 + 7.5 * Math.random() * sinValue, 10);
@@ -133,15 +136,15 @@ app.controller('MainController', function($scope) {
     };
 
 
-    $scope.clearGraph = function() {
-        zingchart.exec('chart-1', 'clearfeed')
+    $scope.clearGraph = function() { //for the clear button
+        zingchart.exec('chart-1', 'clearfeed'); //chart-1 is the id in the html
     }
 
-    $scope.startGraph = function() {
+    $scope.startGraph = function() { //for the start button
         zingchart.exec('chart-1', 'startfeed');
     }
 
-    $scope.stopGraph = function() {
+    $scope.stopGraph = function() { //for the stop button
         zingchart.exec('chart-1', 'stopfeed');
     }
 });
